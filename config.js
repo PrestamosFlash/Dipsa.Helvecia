a996eca809de48d68708fbca02036dec979adbc9
AB <ariadnabode764@gmail.com>
2026-04-07 19:47:46 -0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.DIPSA_CONFIG = {
-  supabaseUrl: 'https://ootycpziyiplekjijsjx.supabase.co',
-  supabaseAnonKey: 'sb_publishable_mbvTCA_kAKIROXyet8NToA_MAL5mKyV',
-  adminPasswordHint: 'Cambiá la clave apenas subas el proyecto.'
-};
